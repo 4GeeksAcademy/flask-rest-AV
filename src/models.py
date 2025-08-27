67b7a251d80e6157dc0b49638a5d86db4ca47112
Ana VR <52681114+Anne1916@users.noreply.github.com>
2025-08-27 22:16:54 +0000

    dbmodelsST_AV

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 db = SQLAlchemy()
 
@@ -10,10 +11,55 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-
+    favorites = relationship("Favorites", back_populates = "user")
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    favorites = relationship("Favorites", back_populates = "planet")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    favorites = relationship("Favorites", back_populates = "character")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable = False)
+    planets_id: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable = True)
+    characters_id: Mapped[int] = mapped_column(ForeignKey("characters.id"), nullable = True)
+    user = relationship("User", back_populates = "favorites")
+    character = relationship("Characters", back_populates = "favorites")
+    planet = relationship("Planets", back_populates = "favorites")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planets_id": self.planets_id,
+            "characters_id": self.characters_id, 
+            
         }
